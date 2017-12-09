69899f7fa4643ed5825639eb573a447f799f2eb4
nickfaelnar <nickfaelnar@gmail.com>
2017-12-10 05:03:38 +0800

    Added main class.

---
+++ src/up/mscs/cs140/galaga/main/GalagaGame.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package up.mscs.cs140.galaga.main;
+
+/**
+ * Main Class
+ */
+public class GalagaGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
